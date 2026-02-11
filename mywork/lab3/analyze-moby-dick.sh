f8936da3ed4cff2e403fb7f38e88d46ff04c6f9c
Sara Elster <selster@me.com>
2026-02-11 14:33:56 -0500

    Overwriting changes to analyze-moby-dick.sh and results.txt

--- mywork/lab3/analyze-moby-dick.sh
+++ mywork/lab3/analyze-moby-dick.sh
@@ -8,7 +8,7 @@ SEARCH_PATTERN="$1"
 OUTPUT="$2"
 
 # Downloading Moby Dick file
-curl -o mobydick.txt https://gist.githubusercontent.com/StevenClontz/4445774/raw/1722a289b665d940495645a5eaaad4da8e3ad4c7/mobydick.txt 
+curl -L -o mobydick.txt https://gist.githubusercontent.com/StevenClontz/4445774/raw/1722a289b665d940495645a5eaaad4da8e3ad4c7/mobydick.txt 
 
 # Counting occurrences of input search pattern
 OCCURRENCES=$(grep -o "$SEARCH_PATTERN" mobydick.txt | wc -l)
