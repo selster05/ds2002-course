474892c73b5a7fd4651b07d7934644626a08dbaf
Sara Elster <selster@me.com>
2026-02-17 13:21:34 -0500

    creating iss.py

---
+++ iss.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# Purpose: build ETL pipeline to extract ISS location data, transform into clean format, and load into CSV file.
+
+# Imports
+import sys
+import os
+import logging
+import requests
+import pandas as pd
+
+# Initializing logger
+
+
+# Extract function
+
+
+# Transform function
+
+
+# Load function
