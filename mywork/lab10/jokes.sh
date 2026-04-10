a84fac3cbf4252d9f1a0439fb4f809873a947fc6
Sara Elster <selsterr@gmail.com>
2026-04-09 23:14:31 -0400

    Complete Lab 10: Containers (Docker on EC2 and Apptainer job array)

---
+++ mywork/lab10/jokes.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#SBATCH --account=ds2002
+#SBATCH --job-name=jokes
+#SBATCH --output=jokes-%j.out
+#SBATCH --error=jokes-%j.err
+#SBATCH --time=00:01:00
+#SBATCH --partition=standard
+#SBATCH --mem=8G
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --array=1-10
+
+module load apptainer
+
+apptainer run ~/lolcow-latest.sif
